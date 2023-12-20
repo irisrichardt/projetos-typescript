bd6080853962fc0fe9c9da888f40ccfa4bbf2448
Íris <iris.balk04@gmail.com>
2023-12-20 09:46:03 -0300

    :sparkles: feat: Exemplos de arrays

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,29 @@
 "use strict";
-let nome = "Íris";
-let idade = 24;
-let maiorIdade = true;
+// -----------------------  VARIÁVEIS -----------------------
+function variaveis() {
+    let nome = "Íris";
+    let idade = 24;
+    let maiorIdade = true;
+    console.log(nome);
+    console.log(idade);
+    console.log(maiorIdade);
+}
 function somar(valor1, valor2) {
     return console.log("Soma:", valor1 + valor2);
 }
-console.log(nome);
-console.log(idade);
-console.log(maiorIdade);
-somar(5, 5);
+// variaveis();
+// somar(5, 5)
+// -----------------------  ARRAYS -----------------------
+function array() {
+    let array = [];
+    array.push(1);
+    array.push(2);
+    console.log(array);
+    let array2 = [10, 15, 20];
+    console.log(array2);
+    let array3 = [100, 150, "teste"];
+    array3.push(1);
+    array3.push("teste 2");
+    console.log(array3);
+}
+// array();
